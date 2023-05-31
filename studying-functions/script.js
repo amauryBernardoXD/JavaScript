c7cd78bb04f74fd8955d2341ddc4178a3c4fc960
Amaury Bernardo <amaury.bernardoo@gmail.com>
2023-05-31 17:51:38 -0300

    functions test

---
+++ studying-functions/script.js
@@ -0,0 +1,13 @@
+function writeMyName (name){
+    return 'My name is: ' + name; 
+}
+
+
+function verifyAge (age){
+    if (verifyAge < 18){
+        console.log(writeMyName('Amaury') + 'Menor')
+    }else{
+        console.log(writeMyName('Amaury') + ' Maior')
+    }
+}
+verifyAge(18)
