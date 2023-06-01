f713b8eeff8ec913197489775210ca8a4e605483
Amaury Bernardo <amaury.bernardoo@gmail.com>
2023-06-01 17:05:49 -0300

    practicing

---
+++ studying-functions/even-or-odd/script.js
@@ -0,0 +1,24 @@
+function evenOrOdd (x){
+    if(x === 1){
+        return 'even';
+    }else if(x === 2){
+        return 'Odd'
+    }else if(x === 3){
+        return 'even'
+    }else if(x === 4){
+        return 'Odd'
+    }else if(x === 5){
+        return 'even'
+    }else if(x === 6){
+        return 'Odd'
+    }else if(x === 7){
+        return 'even'
+    }else if(x === 8){
+        return 'Odd'
+    }else if(x === 9){
+        return 'even'
+    }else if(x === 10){
+        return 'Odd'
+    }                           
+}
+console.log(evenOrOdd(7));
