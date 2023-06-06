d2cc979d0292b598de4685499664604ead058519
Amaury Bernardo <amaury.bernardoo@gmail.com>
2023-06-06 14:11:40 -0300

    testing loop for and while

---
+++ objects/car-class/script.js
@@ -0,0 +1,15 @@
+class Person {
+    name;
+    age;
+
+    constructor (name, age){
+        this.name = name;
+        this.age = age; 
+    }
+}
+
+const person1 = new Person ('Amaury', 31);
+const person2 = new Person ('Roseli', 50)
+
+
+console.log(person2)
