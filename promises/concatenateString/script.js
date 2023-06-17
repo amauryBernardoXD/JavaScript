55239fde7c1e240627cff34065138c337d8d723c
Amaury Bernardo <amaury.bernardoo@gmail.com>
2023-06-17 10:17:36 -0300

    Concatenate Strings

---
+++ promises/concatenateString/script.js
@@ -0,0 +1,16 @@
+function concatStringsDelay (string1, string2){
+    return new Promise ((resolve, reject) => {
+        if(string1 && string2){
+            const result = string1 + string2;
+            resolve(result)
+        }
+    })
+}
+
+concatStringsDelay('Amaury', 'Bernardo')
+
+.then(result => {
+    setTimeout(() => {
+        console.log(result)
+    }, 3000)
+})
